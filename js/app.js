a663850670161cfad9ffbf8afa7de4354e9870cd
Artur <text.test@com>
2024-05-21 19:16:18 +0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+let btnForm = document.querySelector('#comments-form button');
+let countComments = 0;
+let idComment = 0;
+
+btnForm.onclick = function() {
+    idComment++;
+    let form = document.querySelector("#comments-form");
+    if(form.name.value.length < 4) {
+        document.querySelector('#error').innerHTML = "Довжина імя не менше 4 символів";
+        return false;
+    } else if(form.comment.value.length < 10) {
+        document.querySelector('#error').innerHTML = 'Довжина повідомлення не менше 10 символів';
+        return false;
+    }
+
+    document.querySelector('#error').innerHTML = '';
+
+    // Встановимо нове значення для підрахунку коментарів
+
+
+    if(countComments == 0) {    
+        document.querySelector('#comments').innerHTML = "";
+    }
+        countComments++;
+        document.querySelector(".count-comm").innerHTML = countComments;
+
+        let newComment = "<div class='comment' id='block-" + idComment + "'>" +
+            "<span class='delete' onclick='delComm(" + idComment + ")'>&times;</span>" +
+            "<p class='name'>" + form.name.value + "</p>" +
+            "<p class='mess'>" + form.comment.value + "</p>" +
+        "</div>";
+
+        document.querySelector('#comments').insertAdjacentHTML('afterbegin', newComment);
+
+        // Очистка форми
+
+        form.comment.value = "";
+};
+
+function delComm(id) {
+    document.querySelector("#block-" + id).remove();
+
+    countComments--;
+    document.querySelector(".count-comm").innerHTML = countComments;
+
+    if(countComments == 0)
+        document.querySelector('#comments').innerHTML = "Поки коментарів немає";
+}
